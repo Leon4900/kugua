6ce066b4edccfd19caf17125bb0e421b24f40076
梁艳程 <liangyancheng@kingyee.com.cn>
2025-09-01 14:12:38 +0800

    项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class="home">
+    <img class="img" src="@/assets/images/hanhan.jpg" alt="">
+  </div>
+</template>
+
+<style lang='scss' scoped>
+  .home{
+    height: 100%;
+    background-color: #fff;
+  }
+  .img{
+    width: 100%;
+    height: auto;
+    max-width: 750px;
+    margin: 0 auto;
+    display: block;
+  }
+</style>
